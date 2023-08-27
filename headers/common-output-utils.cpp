0ad5a9fe304f34323b9cbc78090ae459d62d2558
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-08-27 17:19:53 -0400

    debug hpp 1

--- headers/common-output-utils.cpp
+++ headers/common-output-utils.cpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <iostream>
 
-void greet_world (std::string name){
-    std::cout
-        << "\n"
-        << "Hello "
-        << name
-        << " I hope you are doing well. :)"
-        << std::endl;
-    return;
+namespace cmout {
+    void greet_world (std::string name){
+        std::cout
+            << "\n"
+            << "Hello "
+            << name
+            << " I hope you are doing well. :)"
+            << std::endl;
+        return;
+    }
 }
--- headers/common-output-utils.hpp
+++ headers/common-output-utils.hpp
@@ -1,5 +1,4 @@
 #ifndef COMMON_OUTPUT_UTILS_HPP
-
 #define COMMON_OUTPUT_UTILS_HPP
 #include <iostream>
 #include <cstring>
@@ -8,15 +7,12 @@
 #include <string>
 #include <vector>
 
-#include "common-output-utils.cpp"
-
-namespace cmout {
-    void greet_world (std::string name);
-    // std::string add_divider (size_t count, char symbol);
-    // std::string add_margin (std::string output_string, size_t top_margin, size_t bottom_margin, char control_character);
-    // void output (std::string value);
-    // std::string character_counter (std::string value);
-    // std::string vector_to_string (std::vector<std::string> value,char divider);
-}
-
+namespace cmout {}
+void greet_world (std::string name);
+// std::string add_divider (size_t count, char symbol);
+// std::string add_margin (std::string output_string, size_t top_margin, size_t bottom_margin, char control_character);
+// void output (std::string value);
+// std::string character_counter (std::string value);
+// std::string vector_to_string (std::vector<std::string> value,char divider);
 #endif
+
